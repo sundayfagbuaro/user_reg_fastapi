7ba46eb9b3aa38fd1c980d5a6b1b1a0f16faf4c7
fabuu4real@gmail.com <fabuu4real@gmail.com>
2025-03-13 11:12:59 +0000

    added project files

---
+++ backend/core/config.py
@@ -0,0 +1,8 @@
+# Creating a class called settings
+class Settings:
+    PROJECT_TITLE: str = "Registration"
+    PROJECT_VERSION: str = "0.1.0"
+
+
+    # Create an instance of the class
+settings = Settings()
